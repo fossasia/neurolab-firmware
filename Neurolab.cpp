c00644f46e41736f5598d1d1020c1bd0916c75a2
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2019-04-07 20:55:02 +0530

    added definitions
    decoupled all definitions from ad7173.h and added them to definitions.h
    all the definitions should define here

--- Neurolab.cpp
+++ Neurolab.cpp
@@ -13,14 +13,15 @@
           Serial.begin(230400);
         
         /* initialize SPI connection to the ADC */
-          AD7173.init();
+          initialize_adc();
 
         /* sync the ADC */
-          AD7173.sync();
+          sync_adc();
       }
 
-    // TO-DO list
+      
     void Neurolab:: updateChannelSettings(void){
+      
       }
     void Neurolab::writeChannelSettings(void){
       }
@@ -35,41 +36,66 @@
     void Neurolab::stopStreaming(void){
       }
 
-
-    //  ADS1299 FUNCITONS
-    void Neurolab::initialize_ad(void){
-      }
-    void Neurolab::printAllRegisters(void){
-      }
-    void Neurolab::updateADchannelSettings(void){
-      }
-    void Neurolab::writeADchannelSettings(void){
-      }
-    void Neurolab::writeADchannelSettings(int){
-      }
-    void Neurolab::activateChannel(int){
-      }
-    void Neurolab::activateChannel(int, byte, byte, boolean){
-      }
-    void Neurolab::deactivateChannel(int){
-      }
-    void Neurolab::configureInternalTestSignal(byte,byte){
-      }
-    void Neurolab::reset_ad(void){
-      }
-    boolean Neurolab::isDataAvailable(void){
-      }
-    void Neurolab::updateChannelData(void){
-      }
-    long Neurolab::getChannel(int){
-      }
-    void Neurolab::putChannel(int, long){
-      }
-    void Neurolab::update24bitData(void){
+/*
+     --------------------------------------------------
+                    ADS1299 FUNCITONS
+     --------------------------------------------------
+*/     
+    void Neurolab::initialize_adc(void){
+     
+      /* initialize SPI connection to the ADC */
+      AD7173.init();
       }
 
-    /* 
-      Here you can implement other functions related to Neurolab like
-      accelerometer, sdcard, wifi module etc..
+    void Neurolab::reset_adc(void){
+      /* reset ADC to default */
+      AD7173.reset();
+      }
+      
+    void Neurolab::sync_adc(void){
+      /* sync the ADC */
+      AD7173.sync();
+      }
 
-    */
+    void Neurolab::get_current_adc_data_channel(adc7173_register_t &channel){
+      /*gets the current conversion results channel*/
+      AD7173.get_current_data_channel(channel);
+      }
+      
+    void Neurolab::set_adc_mode_config(data_mode_t data_mode, clock_mode_t clock_mode){
+      /*sets the ADC mode configuration*/
+      AD7173.set_adc_mode_config(data_mode,clock_mode);
+    }
+    
+    void Neurolab::set_adc_interface_mode_config(bool continuous_read, bool append_status_reg){
+      /* sets the ADC interface mode configuration */
+      AD7173.set_interface_mode_config(continuous_read,append_status_reg);
+      }
+      
+    void Neurolab::get_adc_data(byte *value){
+      /*gets the ADC conversion result*/
+      AD7173.get_data(value);
+      }
+      
+    void Neurolab::set_adc_channel_config(adc7173_register_t channel, bool enable, adc7173_register_t setupc, analog_input_t ain_pos, analog_input_t ain_neg){
+      /* configures ADC channels */
+      AD7173.set_channel_config(channel,enable, setupc, ain_pos, ain_neg);
+      }
+      
+    void Neurolab::set_adc_setup_config(adc7173_register_t setupc, coding_mode_t coding_mode){
+      /*sets the ADC setups coding mode*/
+      AD7173.set_setup_config(setupc ,coding_mode);
+      }
+      
+    void Neurolab::set_adc_filter_config(adc7173_register_t filter, data_rate_t data_rate){
+      /*sets the ADC filters data conversion rate*/
+      AD7173.set_filter_config(filter, data_rate);
+      }
+      
+    void Neurolab::set_adc_gain_registers(adc7173_register_t gain, uint32_t gain_value){
+      /*sets the ADC gain compensation value*/
+      AD7173.set_gain_registers( gain ,gain_value);
+      }
+
+
+    
--- Neurolab.h
+++ Neurolab.h
@@ -2,7 +2,7 @@
 #define ____Neurolab__
 
 #include "AD7173.h"
-
+#include "definitions.h"
 class Neurolab{
 
   public:
@@ -26,29 +26,28 @@ class Neurolab{
 
 
     //  ADS1299 FUNCITONS
-    void initialize_ad(void);
-    void printAllRegisters(void); 
-    void updateADchannelSettings(void);
-    void writeADchannelSettings(void); 
-    void writeADchannelSettings(int);
-    void activateChannel(int);
-    void activateChannel(int, byte, byte, boolean);
-    void deactivateChannel(int);
-    void configureInternalTestSignal(byte,byte);
-    void reset_ad(void);
-    boolean isDataAvailable(void);
-    void updateChannelData(void);
-    long getChannel(int);
-    void putChannel(int, long);
-    void update24bitData(void);
-    
+    void initialize_adc(void);
+    void reset_adc(void);
+    void sync_adc(void);
+    void get_current_adc_data_channel(adc7173_register_t &);
+    void set_adc_mode_config(data_mode_t , clock_mode_t );
+    void set_adc_interface_mode_config(bool , bool );
+    void get_adc_data(byte *);
+    void set_adc_channel_config(adc7173_register_t,bool,adc7173_register_t ,analog_input_t ,analog_input_t );
+    void set_adc_setup_config(adc7173_register_t , coding_mode_t );
+    void set_adc_filter_config(adc7173_register_t , data_rate_t );
+    void set_adc_gain_registers(adc7173_register_t , uint32_t );
+
+
     /* 
       Here you can implement other functions related to Neurolab like
       accelerometer, sdcard, wifi module etc..
 
     */
+    
 };
 
 
 
 #endif
+
