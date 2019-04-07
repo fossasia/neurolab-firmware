611b6994304d4a446ac3bf51834aff1354242895
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2019-04-08 00:08:45 +0530

    neuro_lab class
    Since the neurolab device can be extended not only to measure EEG but also ECG, EKG and accelerometer a separate class is needed to maintain the base class.
    
    NeuroLab class
       |-  ADC
       |- wifi module
       |- accelerometer
    Hope this feature may help to decouple ADC and other modules from the main code
    
    files refactored

---
+++ Neurolab.h
@@ -0,0 +1,48 @@
+#ifndef ____Neurolab__
+#define ____Neurolab__
+
+#include "AD7173.h"
+#include "definitions.h"
+class Neurolab{
+
+  public:
+  
+    AD7173Class ads;
+  
+    // VARIABLES
+    int outputType;
+    int auxData[3];
+    boolean useAux;
+
+    // BOARD WIDE FUNCTIONS
+    void initialize(void);
+    void updateChannelSettings(void);
+    void writeChannelSettings(void);
+    void setChannelsToDefault(void);
+    void sendChannelData(byte);
+    void reportDefaultChannelSettings(void);
+    void startStreaming(void);
+    void stopStreaming(void);
+
+    //  ADS1299 FUNCITONS
+    void initialize_adc(void);
+    void reset_adc(void);
+    void sync_adc(void);
+    void get_current_adc_data_channel(adc7173_register_t &);
+    void set_adc_mode_config(data_mode_t , clock_mode_t );
+    void set_adc_interface_mode_config(bool , bool );
+    void get_adc_data(byte *);
+    void set_adc_channel_config(adc7173_register_t,bool,adc7173_register_t ,analog_input_t ,analog_input_t );
+    void set_adc_setup_config(adc7173_register_t , coding_mode_t );
+    void set_adc_filter_config(adc7173_register_t , data_rate_t );
+    void set_adc_gain_registers(adc7173_register_t , uint32_t );
+
+    /* 
+      Here you can implement other functions related to Neurolab like
+      accelerometer, sdcard, wifi module etc..
+
+    */
+    
+};
+
+#endif
